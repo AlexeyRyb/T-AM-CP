8057a57b5318f948e0e47134dc6757d7e31da1cc
Alexey Rybalkin <alojscha.rybalkin@mail.ru>
2019-03-03 00:30:09 +0300

    add method rest, need test and post

--- checkMCUWIFI/checkMCUWIFI.ino
+++ checkMCUWIFI/checkMCUWIFI.ino
@@ -21,6 +21,32 @@ controlTaMcP tankAM = controlTaMcP(pins);
 int spdLeft = 255;
 int spdRight = 255;
 int per = 10;
+int timeMove = millis();
+int timing = 1;
+bool moving = false;
+
+String valuePost(String reqIn, String valIn)
+{
+  int beginStrIn = reqIn.indexOf(valIn);
+  if (beginStrIn == -1)
+  {
+    return "";
+  }
+  else
+  {
+    int questionNext = reqIn.indexOf("?", beginStrIn);
+    int spaceNext = reqIn.indexOf(" ", beginStrIn);
+    if (questionNext == -1)
+    {
+       return reqIn.substring( beginStrIn + valIn.length() + 1, spaceNext);
+    }
+    else
+    {
+      return reqIn.substring( beginStrIn + valIn.length() + 1, questionNext);
+    }
+    
+  }
+}
 
 void setup() 
 { 
@@ -58,7 +84,10 @@ void setup()
 
 void loop() 
 { 
-
+  if ( ( (millis() - timeMove) / 1000.0 > timing) && moving )
+  {
+    tankAM.moveT(0, 0);
+  }
   WiFiClient client = server.available();
   
   if (!client) 
@@ -76,61 +105,36 @@ void loop()
   
   Serial.println(req);
   client.flush();
-  
-  if (req.indexOf("/gpio/up") != -1)
+
+  if (req.indexOf("GET") != -1)
   {
-  
-    tankAM.moveT(spdLeft, spdRight);
-    
+    int beginStr = req.indexOf("GET") + 5;
+    int endStr = req.lastIndexOf("HTTP") - 1;
+
+    String reqGet = req.substring(beginStr, endStr);
+
+    if (reqGet.indexOf("move") != -1)
+    {
+      int spdLeft = valuePost(reqGet, "left").toInt();
+      int spdRight = valuePost(reqGet, "right").toInt();
+      int timing = valuePost(reqGet, "time").toInt();
+      int timeMove = millis();
+      moving = true;
+      
+      
+      tankAM.moveT(spdLeft, spdRight);
+      
+    }
+
   }
-  else if (req.indexOf("/gpio/down") != -1)
+  if (req.indexOf("POST") != -1)
   {
-    
-    tankAM.moveT(-spdLeft, -spdRight);
-    
+    int beginStr = req.indexOf("POST") + 6;
+    int endStr = req.lastIndexOf("HTTP") - 1;
+
+    String reqPost = req.substring(beginStr, endStr);
   }
-  else if (req.indexOf("/gpio/left") != -1)
-  {
-    
-    tankAM.moveT(-spdLeft, spdRight);
-    
-  } 
-  else if (req.indexOf("/gpio/right") != -1)
-  {
-    
-    tankAM.moveT(spdLeft, spdRight);
-    
-  } 
-  else if (req.indexOf("/gpio/stop") != -1)
-  {
-    
-    tankAM.moveT(0, 0);
-    
-  }
-  else if (req.indexOf("/gpio/moveUpRight") != -1)
-  {
-  
-    tankAM.moveT(spdLeft, 0);
-    
-  }
-  else if (req.indexOf("/gpio/moveUpLeft") != -1)
-  {
-    
-   tankAM.moveT(0, spdRight);
-  
-  }
-  else if (req.indexOf("/gpio/moveBackRight") != -1)
-  {
-  
-    tankAM.moveT(-spdLeft, 0);
-  
-  }
-  else if (req.indexOf("/gpio/moveBackLeft") != -1)
-  {
-  
-    tankAM.moveT(0, -spdRight);
-    
-  }
+ 
   else if (req.indexOf("/gpio/reverse") != -1)
   {
   
@@ -172,9 +176,9 @@ void loop()
   
   String s = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n<!DOCTYPE HTML>\r\n<html>\r\n ";
   
-  s += "<center><a href='http://"+IP+"/gpio/moveUpLeft'>UP AND LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/up'>UP</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/moveUpRight'>UP AND RIGHT</a><br>";
-  s += "<br><a href='http:/"+IP+"/gpio/left'>LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/stop'>STOP</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/right'>RIGHT</a><br>";
-  s += "<br><a href='http://"+IP+"/gpio/moveBackLeft'>DOWN AND LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/down'>DOWN</a> &emsp;&emsp;<a href='http://"+IP+"/gpio/moveBackRight'>DOWN AND RIGHT</a><br>";
+  s += "<center><a href='http://"+IP+"/move?left=0&right=1000&time=5'>UP AND LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/move?left=200&right=1000&time=5'>UP</a> &emsp;&emsp; <a href='http://"+IP+"/move?left=1000&right=0&time=5'>UP AND RIGHT</a><br>";
+  s += "<br><a href='http:/"+IP+"/move?left=-1000&right=1000&time=5'>LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/move?left=0&right=0&time=5'>STOP</a> &emsp;&emsp; <a href='http://"+IP+"/move?left=1000&right=-1000&time=5'>RIGHT</a><br>";
+  s += "<br><a href='http://"+IP+"/gpio/move?left=0&right=-1000&time=5'>DOWN AND LEFT</a> &emsp;&emsp; <a href='http://"+IP+"/move?left=-1000&right=1000&time=5'>DOWN</a> &emsp;&emsp;<a href='http://"+IP+"/gpio/move?left=-1000&right=0&time=5'>DOWN AND RIGHT</a><br>";
   s += "<br><a href='http://"+IP+"/gpio/moveSlow'>SLOW MOVE</a> &emsp;&emsp; <a href='http://"+IP+"/gpio/reverse'>REVERSE</a> &emsp;&emsp; <a href='http:/"+IP+"/gpio/moveFast'>FAST</a> <br>";
   s += "<br>STATUS: <br><br>";
   s += tankAM.getStatus();
